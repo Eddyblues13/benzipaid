08b881fa0c94e57126b3a636daa9b335221016c2
blueswayne133 <blueswayne133@gmail.com>
2025-08-18 18:34:01 -0700

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,6 +19,18 @@
     <title>Benzipaid- Money Transfer & Banking</title>
     <link rel="icon" type="image/png" href="asset/img/favicon.png">
 
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'ec9166ef2b6b5627ef2e32c696441a832b51ed50';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+    </script>
+    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 
 </head>
 
